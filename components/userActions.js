72c6eb7f452201c8cad650d8f66e3e426eca3678
Debojyoti Mishra <105543436+DebojyotiMishra@users.noreply.github.com>
2024-11-04 12:18:02 +0100

    Part 2

--- components/userActions.js
+++ components/userActions.js
@@ -1,24 +1,25 @@
-import edit from '../assets/edit.svg';
-import trash from '../assets/trash.svg';
+import "./userActions.css"
+import { actionButton } from "./actionButton"
+import edit from "../assets/edit.svg"
+import trash from "../assets/trash.svg"
 
 export const usersActions = () => {
 	const actionsContainer = document.createElement("div")
 	actionsContainer.classList.add("actions-container")
 
-	const buttonOne = document.createElement("button")
-	buttonOne.classList.add("btn")
-	const deleteImg = new Image()
-	deleteImg.src = trash
-	buttonOne.appendChild(deleteImg)
-
-	const buttonTwo = document.createElement("button")
-	buttonTwo.classList.add("btn")
-	const editImg = new Image()
-	editImg.src = edit
-	buttonTwo.appendChild(editImg)
+    const buttonOne = actionButton(edit, "warning-btn", buttonOneClick)
+    const buttonTwo = actionButton(trash, "danger-btn", buttonTwoClick)
 
 	actionsContainer.appendChild(buttonOne)
 	actionsContainer.appendChild(buttonTwo)
 
 	return actionsContainer
+}
+
+function buttonOneClick() {
+    console.log("Button One clicked")
+}
+
+function buttonTwoClick() {
+    console.log("Button Two clicked")
 }
